aca560aadae085d8290d51e9172f6031b4c3d2e1
Oliver Schmitz <o.schmitz@uu.nl>
2020-10-28 09:16:41 +0100

    Modified rasterizing point locations

--- source/campo/op_experimental/rasterize.py
+++ source/campo/op_experimental/rasterize.py
@@ -16,8 +16,67 @@ from ..property import Property
 
 
 
-
 def feature_to_raster(field_pset, point_pset):
+
+
+  spatial_ref = osr.SpatialReference()
+  spatial_ref.ImportFromEPSG(28992)
+
+
+  ogr_source=ogr.GetDriverByName('MEMORY').CreateDataSource('memSource')
+
+  tmp_prop = Property('emptycreatename', field_pset.uuid, field_pset.space_domain, field_pset.shapes)
+
+  for idx,area in enumerate(field_pset.space_domain):
+    # Point feature for current location
+    point_x = point_pset.space_domain.xcoord[idx]
+    point_y = point_pset.space_domain.ycoord[idx]
+
+    layername = 'locations'
+    if ogr_source.GetLayerByName(layername):
+      ogr_source.DeleteLayer(layername)
+
+    lyr = ogr_source.CreateLayer(layername, geom_type=ogr.wkbPoint, srs=spatial_ref)
+
+    feat = ogr.Feature(lyr.GetLayerDefn())
+    point = ogr.Geometry(ogr.wkbPoint)
+    point.AddPoint(point_x, point_y)
+    feat.SetGeometry(point)
+
+    lyr.CreateFeature(feat)
+
+    # Raster
+    nr_rows = int(area[4])
+    nr_cols = int(area[5])
+    cellsize = cellsize = math.fabs(area[2] - area[0]) / nr_cols
+
+
+    minX = area[0]
+    maxY = area[3]
+
+
+    target_ds = gdal.GetDriverByName('MEM').Create('', nr_cols, nr_rows, 1, gdal.GDT_Byte)
+    target_ds.SetGeoTransform((minX, cellsize, 0, maxY, 0, -cellsize))
+    target_ds.SetProjection(spatial_ref.ExportToWkt())
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    err = gdal.RasterizeLayer(target_ds, [1], lyr, burn_values=[1], options=['ALL_TOUCHED=TRUE'])
+    gdal.PopErrorHandler()
+
+    band = target_ds.GetRasterBand(1)
+    array = band.ReadAsArray()
+
+    tmp_prop.values()[idx] = array
+
+
+  return tmp_prop
+
+
+
+
+
+
+def feature_to_raster_all(field_pset, point_pset):
 
 
   spatial_ref = osr.SpatialReference()
