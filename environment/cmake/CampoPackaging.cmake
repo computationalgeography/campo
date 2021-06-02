9a5a2c5a7a6988a1d438dca53eece43937d6163f
Oliver Schmitz <o.schmitz@uu.nl>
2021-06-02 08:30:38 +0200

    Packaging added

--- environment/cmake/CampoPackaging.cmake
+++ environment/cmake/CampoPackaging.cmake
@@ -5,13 +5,18 @@ if(UNIX)
 
   set(CPACK_SOURCE_GENERATOR TBZ2)
 
-  set(CPACK_SOURCE_PACKAGE_FILE_NAME "${PROJECT_NAME}-${${PROJECT_NAME}_VERSION}")
+  set(CPACK_SOURCE_PACKAGE_FILE_NAME "${PROJECT_NAME}-${${PROJECT_NAME}_VERSION}-src")
 
   file(READ ${CMAKE_CURRENT_LIST_DIR}/CampoPackagingExclude.cmake _cpack_ignore)
   string(REGEX REPLACE "\n" ";" _cpack_ignore ${_cpack_ignore})
   set(CPACK_SOURCE_IGNORE_FILES "${_cpack_ignore}")
 
   set(CPACK_PACKAGE_CHECKSUM SHA256)
+
+
+  set(CPACK_GENERATOR TBZ2)
+
+  set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-${${PROJECT_NAME}_VERSION}")
 
   include(CPack)
 
